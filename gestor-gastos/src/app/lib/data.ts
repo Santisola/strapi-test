116ca382c37519c587b57f557f89b316bec2b3a9
Santiago Isola <santiago.i@knownonline.com>
2024-04-02 17:06:33 -0300

    Added try catch in get functions

--- gestor-gastos/src/app/lib/data.ts
+++ gestor-gastos/src/app/lib/data.ts
@@ -4,66 +4,74 @@ import { unstable_noStore as noStore } from 'next/cache';
 
 export async function getGastos() {
     noStore();
-    const fetchRes = await fetch(`${STRAPI_ENDPOINT}/gastos?pagination[pageSize]=100&populate=*`)
-    const rta = await fetchRes.json(); 
-
-    const gastos = rta.data.map((gasto:any) => ({
-        item: gasto.attributes.Gasto,
-        valor: gasto.attributes.Valor,
-        fecha: `${gasto.attributes.Fecha.split('-')[2]}-${gasto.attributes.Fecha.split('-')[1]}-${gasto.attributes.Fecha.split('-')[0]}`,
-        categoria: gasto.attributes.tipos_de_gasto.data.attributes.Tipo
-    }));
-  
-    return gastos.sort((a:any, b:any) => {
-        if (a.fecha.split('-')[2] > b.fecha.split('-')[2]) return -1
-        if (a.fecha.split('-')[2] < b.fecha.split('-')[2]) return 1
-
-        if (a.fecha.split('-')[1] > b.fecha.split('-')[1]) return -1
-        if (a.fecha.split('-')[1] < b.fecha.split('-')[1]) return 1
-
-        if (a.fecha.split('-')[0] > b.fecha.split('-')[0]) return -1
-        if (a.fecha.split('-')[0] < b.fecha.split('-')[0]) return 1
-
-        if (a.fecha.split('-')[0] === b.fecha.split('-')[0]) return -1
-    });
+    try {
+        const fetchRes = await fetch(`${STRAPI_ENDPOINT}/gastos?pagination[pageSize]=100&populate=*`)
+        const rta = await fetchRes.json(); 
+    
+        const gastos = rta.data.map((gasto:any) => ({
+            item: gasto.attributes.Gasto,
+            valor: gasto.attributes.Valor,
+            fecha: `${gasto.attributes.Fecha.split('-')[2]}-${gasto.attributes.Fecha.split('-')[1]}-${gasto.attributes.Fecha.split('-')[0]}`,
+            categoria: gasto.attributes.tipos_de_gasto.data.attributes.Tipo
+        }));
+    
+        return gastos.sort((a:any, b:any) => {
+            if (a.fecha.split('-')[2] > b.fecha.split('-')[2]) return -1
+            if (a.fecha.split('-')[2] < b.fecha.split('-')[2]) return 1
+    
+            if (a.fecha.split('-')[1] > b.fecha.split('-')[1]) return -1
+            if (a.fecha.split('-')[1] < b.fecha.split('-')[1]) return 1
+    
+            if (a.fecha.split('-')[0] > b.fecha.split('-')[0]) return -1
+            if (a.fecha.split('-')[0] < b.fecha.split('-')[0]) return 1
+    
+            if (a.fecha.split('-')[0] === b.fecha.split('-')[0]) return -1
+        });
+    } catch (e) {
+        console.error('Error en getGastos', e)
+        return [];
+    }
 }
 
 export async function getGastosPorTipo(mes:string|null=null) {
     noStore();
-    const fetchRes = await fetch(`${STRAPI_ENDPOINT}/tipos-de-gastos?populate=*`)
-    const rta = await fetchRes.json();
-
-    const tiposDeGasto = rta.data.map((tipo:any) => ({
-        id: tipo.id,
-        category: tipo.attributes.Tipo,
-        gastos: tipo.attributes.gastos.data.length > 0 ?
-            tipo.attributes.gastos.data
-                .filter((item:any) => item.attributes.Fecha.split('-')[1] == mes)
-                .map((gasto:any) => ({
-                    item: gasto.attributes.Gasto,
-                    valor: gasto.attributes.Valor
-                }))
-        : []
-    }))
-
-    const orderedGastos = tiposDeGasto.sort((a:any, b:any) => {
-        let totalGastosA = 0;
-        let totalGastosB = 0;
-
-        a.gastos.forEach((gasto:any) => {
-            totalGastosA += gasto.valor;
-        });
-
-        b.gastos.forEach((gasto:any) => {
-            totalGastosB += gasto.valor;
-        });
-
-        return totalGastosB - totalGastosA
-    })
-
-    console.log('GASTOS POR TIPO', orderedGastos);
+    try {
+        const fetchRes = await fetch(`${STRAPI_ENDPOINT}/tipos-de-gastos?populate=*`)
+        const rta = await fetchRes.json();
     
-    return orderedGastos;
+        const tiposDeGasto = rta.data.map((tipo:any) => ({
+            id: tipo.id,
+            category: tipo.attributes.Tipo,
+            gastos: tipo.attributes.gastos.data.length > 0 ?
+                tipo.attributes.gastos.data
+                    .filter((item:any) => item.attributes.Fecha.split('-')[1] == mes)
+                    .map((gasto:any) => ({
+                        item: gasto.attributes.Gasto,
+                        valor: gasto.attributes.Valor
+                    }))
+            : []
+        }))
+    
+        const orderedGastos = tiposDeGasto.sort((a:any, b:any) => {
+            let totalGastosA = 0;
+            let totalGastosB = 0;
+    
+            a.gastos.forEach((gasto:any) => {
+                totalGastosA += gasto.valor;
+            });
+    
+            b.gastos.forEach((gasto:any) => {
+                totalGastosB += gasto.valor;
+            });
+    
+            return totalGastosB - totalGastosA
+        })
+          
+        return orderedGastos;
+    } catch (e) {
+        console.error('Error en getGastosPorTipo', e)
+        return [];
+    }
 }
 
 export function getMesesConGastos(gastos:any) {
